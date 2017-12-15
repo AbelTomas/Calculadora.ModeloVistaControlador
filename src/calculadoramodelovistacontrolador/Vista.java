b2380c50981d35038bbce8ba1d8972ce16c7884b
AbelTomas <34541232+AbelTomas@users.noreply.github.com>
2017-12-15 11:23:31 +0100

    Vista creada

--- src/calculadoramodelovistacontrolador/Vista.java
+++ src/calculadoramodelovistacontrolador/Vista.java
@@ -5,10 +5,36 @@
  */
 package calculadoramodelovistacontrolador;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Panel;
+import java.awt.TextField;
+
 /**
  *
  * @author Abel
  */
-public class Vista {
-    
+public class Vista extends Frame{
+
+    public Vista() {
+        Panel p=new Panel(new GridLayout(4,4));
+        TextField texto=new TextField();
+        this.add(texto,BorderLayout.NORTH);
+        this.add(p);
+        
+        for (int i = 0; i <= 9; i++) {
+            p.add(new Button(""+i));
+        }
+        
+        p.add(new Button("-"));
+        p.add(new Button("/"));
+        p.add(new Button("*"));
+        p.add(new Button("+"));
+        p.add(new Button("="));
+        p.add(new Button("BORRAR"));
+        this.setBounds(50,50, 300, 300);
+        this.setVisible(true);        
+    } 
 }
